fe2758f50c19fa2615b49cf8c0e1469ce1cf67a3
roboblox <supeecyborg@gmail.com>
2020-06-05 19:54:00 -0400

    finished step 1 and 2

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -18,40 +18,70 @@
 // </div>
 //
 // Use your function to create a card for each of the articles and add the card to the DOM.
+const cardsContainer = document.querySelector(".cards-container");
+function cardMaker(object) {
+  const card = document.createElement("div");
+  const headline = document.createElement("div");
+  const author = document.createElement("div");
+  const imageContainer = document.createElement("div");
+  const image = document.createElement("img");
+  const authorName = document.createElement("span");
 
-// axios
-//   .get("https://lambda-times-backend.herokuapp.com/articles")
-//   .then((response) => {
-//     this.addresses = response.data.map((country) => country.name);
-//     console.log(
-//       "the response from the API, organized for us by axios",
-//       response.data
-//     );
-//   })
-//   .catch((error) => {
-//     console.log(
-//       "something went wrong, hopefully the error tells us what",
-//       error
-//     );
-//   });
-// function cardMaker() {
-//   const card = document.createElement("div");
-//   const headline = document.createElement("div");
-//   const author = document.createElement("div");
-//   const imageContainer = document.createElement("div");
-//   const image = document.createElement("img");
-//   const authorName = document.createElement("span");
+  card.classList.add("card");
+  headline.classList.add("headline");
+  author.classList.add("author");
+  imageContainer.classList.add("img-container");
 
-//   card.classList.add("card");
-//   headline.classList.add("headline");
-//   author.classList.add("author");
-//   imageContainer.classList.add("img-container");
+  // card.textContent = object;
+  // headline.textContent = object;
+  authorName.textContent = object.authorName;
+  // card.appendChild(headline);
+  card.appendChild(authorName);
+  return card;
+}
+function cardMakerPlus() {
+  const axiosy = axios.get(
+    "https://lambda-times-backend.herokuapp.com/articles"
+  );
 
-//   card.textContent = object;
-// }
-// You can use `array.map` to take only the names like this:
+  axiosy
+    .then((response) => {
+      // const
+      const carder = response.data.articles.javascript;
 
-//     axios.get('api_link', {})
-//       .then((response) => {
-//         this.addresses = response.data.map(country => country.name)
-//       })
+      // console.log(carder.javascript[1].authorName);
+      console.log(carder);
+      // let { javascipt, bootstrap, technology, jquery, node } = carder;
+      // console.log(carder);
+
+      carder1 = Object.entries(response.data.articles.javascript);
+      console.log(carder1);
+      for (let prop in carder1) {
+        console.log(carder1[prop]);
+      }
+
+      carder.forEach((element) => {
+        console.log("Hello");
+        console.log(element);
+
+        const card = cardMaker(element);
+        const card2 = response.data.articles[0];
+
+        cardsContainer.appendChild(card);
+        console.log(cardsContainer);
+      });
+
+      console.log(
+        "the response from the API, organized for us by axios",
+        response.data
+      );
+    })
+    .catch((error) => {
+      console.log(
+        "something went wrong, hopefully the error tells us what",
+        error
+      );
+    });
+  return "Hello";
+}
+cardMakerPlus();
