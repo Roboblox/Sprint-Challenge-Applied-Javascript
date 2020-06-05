75952e0be58a1141c72a2e043b365e1344290b6b
roboblox <supeecyborg@gmail.com>
2020-06-05 14:01:05 -0400

    not finished

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -18,3 +18,40 @@
 // </div>
 //
 // Use your function to create a card for each of the articles and add the card to the DOM.
+
+axios
+  .get("https://lambda-times-backend.herokuapp.com/articles")
+  .then((response) => {
+    this.addresses = response.data.map((country) => country.name);
+    console.log(
+      "the response from the API, organized for us by axios",
+      response.data
+    );
+  })
+  .catch((error) => {
+    console.log(
+      "something went wrong, hopefully the error tells us what",
+      error
+    );
+  });
+function cardMaker() {
+  const card = document.createElement("div");
+  const headline = document.createElement("div");
+  const author = document.createElement("div");
+  const imageContainer = document.createElement("div");
+  const image = document.createElement("img");
+  const authorName = document.createElement("span");
+
+  card.classList.add("card");
+  headline.classList.add("headline");
+  author.classList.add("author");
+  imageContainer.classList.add("img-container");
+
+  card.textContent = object;
+}
+// You can use `array.map` to take only the names like this:
+
+//     axios.get('api_link', {})
+//       .then((response) => {
+//         this.addresses = response.data.map(country => country.name)
+//       })
--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,89 @@
 //
 //  Each tab should look like this:
 //    <div class="tab">topic here</div>
+// axios
+//   .get("https://lambda-times-backend.herokuapp.com/topics")
+//   .then((response) => {
+//     console.log(
+//       "the response from the API, organized for us by axios",
+//       response.data
+//     );
+//   })
+//   .catch((error) => {
+//     console.log(
+//       "something went wrong, hopefully the error tells us what",
+//       error
+//     );
+//   });
+// function tabMaker(object) {
+//   const tab = document.createElement("div");
+
+//   tab.classList.add("tab");
+
+//   tab.textContent = object.topics;
+
+//   //   document.querySelector(".topics").appendChild(tab);
+
+//   return tab;
+// }
+axios
+  .get("https://lambda-times-backend.herokuapp.com/topics/")
+  .then((response) => {
+    this.tab = response.data.map(function (item) {
+      console.log(item);
+    });
+
+    // let topic = tabMaker(reponse.data);
+    // console.log(topic);
+    // document.querySelector(".topics").appendChild(topic);
+
+    console.log(
+      "the response from the API, organized for us by axios",
+      response.data
+    );
+  })
+  .catch((error) => {
+    console.log(
+      "something went wrong, hopefully the error tells us what",
+      error
+    );
+  });
+// let topics = axios
+//   .get(element)
+//   .then((response) => {
+//     for (let i = 0; i < 5; i++) {
+//       tab = tabMaker(response.data);
+//       document.querySelector(".topics").appendChild(tab);
+//       return tab;
+//     }
+//     console.log(
+//       "the response from the API, organized for us by axios",
+//       response.data
+//     );
+//   })
+//   .catch((error) => {
+//     console.log(
+//       "something went wrong, hopefully the error tells us what",
+//       error
+//     );
+//   });
+// axios
+//   .get(element)
+//   .then((response) => {
+//     for (let i = 0; i < 5; i++) {
+//       tab = tabMaker(response.data);
+//       document.querySelector(".topics").appendChild(tab);
+//       return tab;
+//     }
+//     console.log(
+//       "the response from the API, organized for us by axios",
+//       response.data
+//     );
+//   })
+//   .catch((error) => {
+//     console.log(
+//       "something went wrong, hopefully the error tells us what",
+//       error
+//     );
+//   });
+// console.log(topics);
