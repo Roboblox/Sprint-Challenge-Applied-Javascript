b37828aa4e0610c3c6e4d03c3902313bf01d9ca6
roboblox <supeecyborg@gmail.com>
2020-06-07 12:05:11 -0400

    finished without questions answered

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -18,91 +18,25 @@
 // </div>
 //
 // Use your function to create a card for each of the articles and add the card to the DOM.
-const cardsContainer = document.querySelector(".cards-container");
-const card = document.querySelector("card");
-const author = document.createElement("div");
 
-function cardMaker(object) {
-  const headline = document.createElement("div");
-
-  const imageContainer = document.createElement("div");
-  const image = document.createElement("img");
-  const authorName = document.createElement("span");
-
-  headline.classList.add("headline");
-  author.classList.add("author");
-  imageContainer.classList.add("img-container");
-
-  author.textContent = object;
-  // headline.textContent = object;
-  authorName.textContent = object.authorName;
-  // headline.textContent = object.headline;
-  image.textContent = object.authorPhoto;
-  // authorName.textContent = object.authorName;
-
-  // card.appendChild(headline);
-  author.appendChild(authorName);
-  author.appendChild(imageContainer);
-  imageContainer.appendChild(image);
-  return author;
-}
-// function cardMakerPlus() {
-const axiosy = axios.get("https://lambda-times-backend.herokuapp.com/articles");
-
-axiosy
+axios
+  .get("https://lambda-times-backend.herokuapp.com/articles")
   .then((response) => {
-    // const
     const carder = response.data.articles;
     console.log(carder);
-    const keys = Object.keys(carder);
-    console.log(keys);
-    keys.forEach((element) => {
-      console.log(element);
-      const card = document.createElement("div");
-      card.classList.add("card");
-      card.textContent = element;
-      cardsContainer.appendChild(card);
-      console.log(cardsContainer);
-    });
-
-    let text = [];
-    for (let key in carder) {
-      text.push(carder[key]);
+    const keys = Object.entries(carder);
+    for (let i = 0; i < keys.length; i++) {
+      console.log(keys);
+      console.log(keys[i]);
+      for (let j = 0; j < keys[i][1].length; j++) {
+        console.log(keys[i][1].length);
+        console.log(keys[i][1][j]);
+        cardsContainer = document.querySelector(".cards-container");
+        const cards = cardMaker(keys[i][1][j]);
+        cardsContainer.appendChild(cards);
+        console.log(".cards-container");
+      }
     }
-    console.log(text);
-
-    for (let i = 0; i < text.length; i++) {
-      console.log(text[i]);
-      const author = cardMaker(text[i]);
-      text[i].forEach((element) => {
-        console.log(element);
-        const author = cardMaker(element);
-        // const headline = cardMaker(element);
-        //  const image = cardMaker(element);
-        console.log(author);
-        console.log(cardsContainer);
-      });
-    }
-    // text1.forEach((element) => {
-    //   console.log(element);
-    // });
-
-    //   // const card = cardMaker(carder);
-    //   // console.log(card);
-    // }
-    // let { javascipt, bootstrap, technology, jquery, node } = carder;
-
-    // keys.forEach((element) => {
-    //   console.log(element);
-    //   const card = cardMaker(element);
-
-    // });
-    // values.forEach((element) => {
-    //   // console.log(element.authorName);
-    //   // const card = cardMaker(element.authorName);
-    //   // cardsContainer.append(card);
-    //   // console.log(cardsContainer);
-    // });
 
     console.log(
       "the response from the API, organized for us by axios",
@@ -115,6 +49,81 @@ axiosy
       error
     );
   });
-// return "Hello";
+
+function cardMaker(object) {
+  const card = document.createElement("div");
+  const imageContainer = document.createElement("div");
+  const image = document.createElement("img");
+  const authorName = document.createElement("span");
+  const author = document.createElement("div");
+  const headline = document.createElement("div");
+
+  author.classList.add("author");
+  headline.classList.add("headline");
+  imageContainer.classList.add("img-container");
+  card.classList.add("card");
+
+  headline.textContent = object.headline;
+  authorName.textContent = object.authorName;
+  image.src = object.authorPhoto;
+
+  card.appendChild(headline);
+  card.appendChild(author);
+  author.appendChild(authorName);
+  author.appendChild(imageContainer);
+  imageContainer.appendChild(image);
+  return card;
+}
+
+// keys.forEach((element) => {
+//   console.log(element);
+//   const card = document.createElement("div");
+//   card.classList.add("card");
+//   card.textContent = element;
+//   cardsContainer.appendChild(card);
+//   console.log(cardsContainer);
+
+// });
+
+//   for (let n = 0; n < keys[i].length; i++) {
+//     console.log(keys[i][1]);
+//     // const headline = document.createElement("div");
+//     // headline.classList.add("headline");
+//     // headline.textContent = obj[n][i].headline;
+//     // card.appendChild(headline);
+//     // console.log(card);
+//   }
 // }
-// cardMakerPlus();
+
+// console.log(keys[i][0].length);
+
+// //  const image = cardMaker(element);
+// console.log(author);
+// console.log(cardsContainer);
+
+// let text = [];
+// for (let key in carder) {
+//   text.push(carder[key]);
+// }
+// console.log(text.length);
+
+// for (let i = 0; i < text.length; i++) {
+//   console.log(text[i].length);
+
+//   text[i].forEach((element) => {
+//     console.log(element);
+
+//   });
+// }
+// text1.forEach((element) => {
+//   console.log(element);
+// });
+
+//   // const card = cardMaker(carder);
+//   // console.log(card);
+// }
+// let { javascipt, bootstrap, technology, jquery, node } = carder;
+
+// keys.forEach((element) => {
+//   console.log(element);
+//   const card = cardMaker(element);
--- components/Header/index.js
+++ components/Header/index.js
@@ -1,33 +1,33 @@
-// // STEP 1: Create a Header component.
-// // -----------------------
-// // Write a function that returns the markup you see below:
-// //
-// //  <div class="header">
-// //    <span class="date">SMARCH 28, 2019</span>
-// //    <h1>Lambda Times</h1>
-// //    <span class="temp">98°</span>
-// //  </div>
-// //
-// // Use your function to create a header
-// // and append it to the DOM inside the div.header-container
+// STEP 1: Create a Header component.
+// -----------------------
+// Write a function that returns the markup you see below:
+//
+//  <div class="header">
+//    <span class="date">SMARCH 28, 2019</span>
+//    <h1>Lambda Times</h1>
+//    <span class="temp">98°</span>
+//  </div>
+//
+// Use your function to create a header
+// and append it to the DOM inside the div.header-container
 
-// function Header() {
-//   const headerContainer = document.querySelector(".header-container");
-//   const header = document.createElement("div");
-//   const date = document.createElement("span");
-//   const h1 = document.createElement("h1");
-//   const temp = document.createElement("span");
+function Header() {
+  const headerContainer = document.querySelector(".header-container");
+  const header = document.createElement("div");
+  const date = document.createElement("span");
+  const h1 = document.createElement("h1");
+  const temp = document.createElement("span");
 
-//   header.classList.add("header");
-//   date.classList.add("date");
-//   temp.classList.add("temp");
+  header.classList.add("header");
+  date.classList.add("date");
+  temp.classList.add("temp");
 
-//   date.textContent = "SMARCH 28, 2019";
-//   h1.textContent = "Lambda Times";
-//   temp.textContent = "98°";
+  date.textContent = "SMARCH 28, 2019";
+  h1.textContent = "Lambda Times";
+  temp.textContent = "98°";
 
-//   headerContainer.append(header);
-//   header.append(date, h1, temp);
-//   return headerContainer;
-// }
-// console.log(Header());
+  headerContainer.append(header);
+  header.append(date, h1, temp);
+  return headerContainer;
+}
+Header();
